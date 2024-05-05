54dfacca159674d62f41d45682a3653c9c8cc4ca
gruler <gruler@me.com>
2024-05-04 17:05:30 -0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -114,6 +114,7 @@ router.post('/register', (req, res) => {
         }
         if (isMatch) {
           // Passwords match
+          req.session.user = results[0]; // Store the whole user object or just parts of it, e.g., req.session.user = { id: results[0].id, name: results[0].name };
           return res.redirect('/dashboard'); // or wherever you want the user to go after login
         } else {
           // Passwords do not match
