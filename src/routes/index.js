281e1a4d242339e819fdd27d69e4f4f587dd19fd
gruler <gruler@me.com>
2024-05-06 12:40:32 -0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -188,7 +188,7 @@ router.post('/login', (req, res) => {
       if (results.length === 0) {
         return res.status(404).send('User not found');
       }
-      console.log('emailverified:',results.emailverified);
+      console.log('results:',results[0].emailverified);
       if (results[0].emailverified === 0) {
       //  return res.status(404).send('Please verify your email before logging in.');
          // Redirect to login with a logout message
