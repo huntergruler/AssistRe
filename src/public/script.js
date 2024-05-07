6e6468f597d17b226a27c0d4a1372e8d1bc387cd
gruler <gruler@me.com>
2024-05-07 16:59:09 -0700

    more

--- src/public/script.js
+++ src/public/script.js
@@ -173,10 +173,6 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value.trim();
-    var messageElement = document.getElementById('Message');
-    messageElement.innerText = 'Updated content!';
-    document.getElementById('message').innerText = 'GOT HERE';
-    document.getElementById('message').style.color = 'green'; // Optional: change text color
 
     if (password.length < 4) {
         document.getElementById('message').innerText = 'Password must be at least 4 characters long.';
