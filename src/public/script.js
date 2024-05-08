4c5c26d346826ba996d52c6ad3d83ca398a1c509
gruler <gruler@me.com>
2024-05-08 06:47:51 -0700

    Update script.js

--- src/public/script.js
+++ src/public/script.js
@@ -105,7 +105,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value.trim();
 
-    fetch('/route/login', {
+    fetch('/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
