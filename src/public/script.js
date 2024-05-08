d8b184365354ba2fee9e3bd1c0f7e7a4e7713104
gruler <gruler@me.com>
2024-05-08 07:30:16 -0700

    Update script.js

--- src/public/script.js
+++ src/public/script.js
@@ -105,6 +105,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value.trim();
 
+    console.log(data);
     fetch('/login', {
         method: 'POST',
         headers: {
