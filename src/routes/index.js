284cb84e4b1a1e3a53151e4aea5000ddd3938b29
gruler <gruler@me.com>
2024-05-13 16:10:27 -0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -361,7 +361,8 @@ router.get('/reset-password', (req, res) => {
   const { token } = req.query;
   // Verify the token and its expiration
   const query = 'SELECT * FROM Users WHERE resetToken=? AND resetTokenExpire > ?';
-  console.log('Token:', token, 'Now:', Date.now().toString());
+  const date = new Date(Date.now())
+  console.log('Token:', token, 'Now:', date.toString());
   db.query(query, [token, Date.now()], (error, results) => {
     if (error || results.length === 0) {
       return res.status(400).send('Invalid or expired token');
