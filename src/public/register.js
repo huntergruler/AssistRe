577c1dfa6aafe2589e1b2b8ecc70d0e5ce24a570
gruler <gruler@me.com>
2024-05-13 17:16:38 -0700

    changes

--- src/public/register.js
+++ src/public/register.js
@@ -1,4 +1,25 @@
-document.addEventListener('keyup', function(event) {
+function lookupCityState() {
+  console.log('lookupCityState');
+  let zipCode = document.getElementById('zipCode').value;
+  if (zipCode) {
+      // Create a new XMLHttpRequest object
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', '/get-city-state?zipCode=' + zipCode, true);
+      xhr.onreadystatechange = function () {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+              let response = JSON.parse(xhr.responseText);
+              if (response.city && response.state) {
+                  document.getElementById('cityState').textContent = response.city + ', ' + response.state;
+              } else {
+                  document.getElementById('cityState').textContent = 'City and state not found';
+              }
+          }
+      };
+      xhr.send();
+  }
+}
+
+document.addEventListener('blur', function(event) {
     if (event.target.id === 'user') {
         const User = document.getElementById('user').value.trim();
         fetch(`/check-user?username=${encodeURIComponent(User)}`)
