993b968440a3387767720d1913d90fd72da218b3
gruler <gruler@me.com>
2024-05-14 11:57:33 -0700

    working on license delete

--- src/public/script.js
+++ src/public/script.js
@@ -19,7 +19,7 @@ function addLicense() {
     .then(data => {
         const table = document.getElementById('licensesTable').getElementsByTagName('tbody')[0];
         const row = table.insertRow();
-        row.innerHTML = `<td>${data.licensenumber}</td><td>${data.licensestate}</td><td>${data.agentlicenseid}</td><td></button><button onclick="deleteLicense(${data.id})">Delete</button></td>`;
+        row.innerHTML = `<td>${data.licensenumber}</td><td>${data.licensestate}</td><td>${data.agentlicenseid}</td><td></button><button onclick="deleteLicense(${data.agentlicenseid})">Delete</button></td>`;
         document.getElementById('licenseForm').reset();
     })
     .catch(error => console.error('Error:', error));
