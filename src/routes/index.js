d0f084850f78627627f5c41ff676ee8367fb56c2
gruler <gruler@me.com>
2024-05-16 12:14:36 -0700

    fixed select statements for less processing

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,14 +12,11 @@ const crypto = require('crypto');
 const { body, validationResult } = require('express-validator');
 const saltRounds = 10; // The cost factor controls how much time is needed to calculate a single bcrypt hash.
 const bodyParser = require('body-parser');
-const profileRoutes = require('./profile');
 
 // Ensure you have body-parser configured to parse JSON
 router.use(bodyParser.json());
 router.use(express.json());
 router.use(cookierParser());
-
-router.use('/profile', profileRoutes);
 
 // Database connection setup
 const db = mysql.createConnection({
@@ -92,6 +89,20 @@ router.post('/register', (req, res) => {
    });
   });
 
+  // Assuming `db` is your MySQL connection db, already set up in app.js
+  router.get('/profile', (req, res) => {
+    if (!req.session.user) {
+      req.session.message = 'Please login to access the Profile';
+      res.redirect('/login');  
+    }
+          const query = 'SELECT a.agentlicenseid, date_format(a.licenseExpirationDate,"%m/%d/%Y") licenseExpirationDate, a.licenseNumber, a.licenseState, a.userid FROM AgentLicenses a where userid = ?'; 
+      db.query(query,[ req.session.userid ], (err, results) => {
+          if (err) throw err;
+          let hasLicenses = results.length > 0;
+          res.render('profile', { licenses: results, hasLicenses: hasLicenses, user: req.session.user, firstname: req.session.firstname, userid: req.session.userid, lastname: req.session.lastname});
+        });
+  });
+  
   router.post('/api/licenses', (req, res) => {
       const { licenseNumber, licenseState, licenseExpirationDate } = req.body;
       const insertQuery = 'INSERT INTO AgentLicenses (licenseNumber, licenseState, licenseExpirationDate, userid) VALUES (?, ?, ?, ?)';
