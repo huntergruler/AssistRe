4eca33e4e9e1f99e7fd6b456bba80064a3b4b188
gruler <gruler@me.com>
2024-05-16 13:27:44 -0700

    fixed select statements for less processing

--- src/routes/index.js
+++ src/routes/index.js
@@ -363,7 +363,7 @@ router.post('/process-zip-codes', (req, res) => {
 
 router.post('/api/offices', (req, res) => {
   const { officeName, address, city, state, zip, phoneNumber, officeLicenseNumber, officeLicenseState, userid } = req.body;
-  const insertQuery = 'INSERT INTO Offices (officeName, address, city, state, zip, phoneNumber, officeLicenseNumber, officeLicenseState, userid) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  const insertQuery = 'INSERT INTO AgentOffices (officeName, address, city, state, zip, phoneNumber, officeLicenseNumber, officeLicenseState, userid) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
   db.query(insertQuery, [officeName, address, city, state, zip, phoneNumber, officeLicenseNumber, officeLicenseState, userid], (err, result) => {
       if (err) throw err;
       res.json({ officeName, address, city, state, zip, phoneNumber, officeLicenseNumber, officeLicenseState, userid });
