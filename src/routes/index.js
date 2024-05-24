be84e4724de9726e56ca2f1dd5c3c2a0cb8464ba
gruler <gruler@me.com>
2024-05-24 08:16:41 -0700

    fixed select statements for less processing

--- src/routes/index.js
+++ src/routes/index.js
@@ -242,7 +242,7 @@ router.post('/login', [
     res.setHeader('Content-Type', 'application/json');
     db.query(userQuery, [email], (error, results) => {
       console.log('Results:', results);
-      if (results.length === 0) { 
+      if (!results) { 
         // Send response when email is not found
         res.json({
           success: false,
