e4c3d93c96a88f38a07335d211d30d2404dbf26f
gruler <gruler@me.com>
2024-05-23 21:50:14 -0700

    fixed select statements for less processing

--- src/public/register.js
+++ src/public/register.js
@@ -1,50 +1,49 @@
 function lookupCityState() {
-    let zipCode = document.getElementById('zipCode').value;
-    if (zipCode) {
-        // Create a new XMLHttpRequest object
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', '/get-city-state?zipCode=' + zipCode, true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                let response = JSON.parse(xhr.responseText);
-                if (response.city && response.state) {
-                    document.getElementById('cityState').textContent = response.city + ', ' + response.state;
-                } else {
-                    document.getElementById('cityState').textContent = 'City and state not found';
-                }
-            }
-        };
-        xhr.send();
-    }
+  let zipCode = document.getElementById('zipCode').value;
+  if (zipCode) {
+      // Create a new XMLHttpRequest object
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', '/get-city-state?zipCode=' + zipCode, true);
+      xhr.onreadystatechange = function () {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+              let response = JSON.parse(xhr.responseText);
+              if (response.city && response.state) {
+                  document.getElementById('cityState').textContent = response.city + ', ' + response.state;
+              } else {
+                  document.getElementById('cityState').textContent = 'City and state not found';
+              }
+          }
+      };
+      xhr.send();
+  }
 }
 //Test
-document.addEventListener('keyup', function (event) {
-    if (event.target.id === 'user') {
-        const User = document.getElementById('user').value.trim();
-        console.log(User);
-        fetch(`/check-user?email=${encodeURIComponent(User)}`)
-            .then(response => response.json())
-            .then(data => {
-                if (data.available) {
-                    // Email is available
-                    document.getElementById('userStatus').textContent = 'Email is available.';
-                    document.getElementById('userStatus').style.color = 'green';
-                } else {
-                    // Email is not available
-                    document.getElementById('userStatus').textContent = 'Email is already registered.';
-                    document.getElementById('userStatus').style.color = 'red';
-                    document.getElementById('user').focus();
-                }
-            })
-            .catch(error => console.error('Error checking user:', error));
+document.addEventListener('keyup', function(event) {
+    if (event.target.id === 'email') {
+        const Email = document.getElementById('email').value.trim();
+        console.log(Email);
+        fetch(`/check-user?email=${encodeURIComponent(Email)}`)
+        .then(response => response.json())
+        .then(data => {
+          if (data.available) {
+            // Email is available
+            document.getElementById('userStatus').textContent = 'Email is available.';
+            document.getElementById('userStatus').style.color = 'green';
+          } else {
+            // Email is not available
+            document.getElementById('userStatus').textContent = 'Email is already registered.';
+            document.getElementById('userStatus').style.color = 'red';
+            document.getElementById('email').focus();
+        }
+        })
+        .catch(error => console.error('Error checking email:', error));
     }
 }, true); // Using capturing phase to handle the event as it propagates down
 
 document.querySelectorAll('input[name="userType"]').forEach(radio => {
-    radio.addEventListener('change', function () {
-        const email = document.getElementById('email').value.trim();
+    radio.addEventListener('change', function() {
         document.querySelectorAll('#registerForm input, #registerForm button').forEach(field => {
             field.disabled = false;
         });
     });
-})
+});
