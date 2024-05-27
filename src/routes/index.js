c134e281ba4f8e4f9227995942b7662912247f6e
gruler <gruler@me.com>
2024-05-26 19:41:17 -0700

    working on license delete

--- src/routes/index.js
+++ src/routes/index.js
@@ -155,8 +155,8 @@ router.post('/profile_b_property', (req, res) => {
   }
   else {
 
-  const { bathrooms, bedrooms, buyerType, preferredLanguages, prequalified, priceRange, propertyType, squareFootage, timeFrame, userPhoto, userid } = req.body;
-console.log('Params:', [bathrooms, bedrooms, buyerType, preferredLanguages, prequalified, priceRange, propertyType, squareFootage, timeFrame, userPhoto, userid] )
+  const { bathrooms_min, bathrooms_max bedrooms_min, bedrooms_max buyerType, preferredLanguages, prequalified, price_min, price_max, propertyType, squareFootage_min, squareFootage_max, timeFrame, userPhoto, userid } = req.body;
+  console.log('bathrooms_min:', bathrooms_min, 'bathrooms_max:', bathrooms_max, 'bedrooms_min:', bedrooms_min, 'bedrooms_max:', bedrooms_max, 'buyerType:', buyerType, 'preferredLanguages:', preferredLanguages, 'prequalified:', prequalified, 'price_min:', price_min, 'price_max:', price_max, 'propertyType:', propertyType, 'squareFootage_min:', squareFootage_min, 'squareFootage_max:', squareFootage_max, 'timeFrame:', timeFrame, 'userPhoto:', userPhoto, 'userid:', userid );
   const query = 'UPDATE Buyers SET bathrooms_min = ?, bathrooms_max = ?, bedrooms_min = ?, bedrooms_max = ?, buyerType = ?, preferredLanguages = ?, prequalified = ?, price_min = ?, price_max = ?, propertyType = ?, squareFootage_min = ?, squareFootage_max = ?, timeFrame = ?, userPhoto = ? WHERE userid = ?';
   db.query(query, [bathrooms_min, bathrooms_max, bedrooms_min, bedrooms_max, buyerType, preferredLanguages, prequalified, price_min, price_max, propertyType, squareFootage_min, squareFootage_max, timeFrame, userPhoto, userid], (error, results) => {
     if (error) {
