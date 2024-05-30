aa614afbc488a08fb8c91461bdb0fd7c8500b892
gruler <gruler@me.com>
2024-05-30 08:18:04 -0700

    working on sliders

---
+++ src/public/zipcodetest.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+    populateUserZipCodes();
+    //    populateStates();
+});
+
+function populateUserZipCodes() {
+    const selectedZipCodesContainer = document.getElementById("selectedZipCodesContainer");
+    const ownedZipCodes = document.getElementById("ownedZipCodes");
+    let htmlCodes = '';
+    console.log('populateUserZipCodes');
+    fetch(`/get-agentzipcodes`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if (selectedZipCodesContainer) {
+                selectedZipCodesContainer.innerHTML = '';
+            }
+            if (data.error) {
+                const div = document.createElement("div");
+                div.textContent = 'No zip codes selected';
+                if (selectedZipCodesContainer) {
+                    selectedZipCodesContainer.appendChild(div);
+                }
+                htmlCodes += `<p>No Zip Codes</p><br>`;
+            }
+            else {
+                data.results.forEach(code => {
+                    const div = document.createElement("div");
+                    div.textContent = code.zipCode;
+                    div.className = "form-group row justify-content-center";
+                    div.onclick = function () {
+                        this.classList.toggle("selected");
+                    };
+                    if (selectedZipCodesContainer) {
+                        selectedZipCodesContainer.appendChild(div);
+                    }
+                    htmlCodes += `<p>${code.zipCode} - ${code.city}, ${code.state}</p><br>`;
+                });
+            }
+            if (ownedZipCodes) {
+                ownedZipCodes.innerHTML = htmlCodes;
+            }
+        })
+        .catch(error => console.error('Error checking user:', error));
+};
