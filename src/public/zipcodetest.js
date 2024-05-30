15cdeb18a19c6f59e387f85b866c8ea389de7911
gruler <gruler@me.com>
2024-05-30 12:14:45 -0700

    working on sliders

--- src/public/zipcodetest.js
+++ src/public/zipcodetest.js
@@ -17,7 +17,7 @@ function populateUserZipCodes() {
             }
             if (data.error) {
                 const div = document.createElement("div");
-                div.className = "form-group row justify-content-center";
+                div.className = "ownedZipCodes row justify-content-center";
                 div.textContent = 'No zip codes selected';
                 if (selectedZipCodesContainer) {
                     selectedZipCodesContainer.appendChild(div);
