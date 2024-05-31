9b1b64002f91d10fc93b60858ef14f2328e4bc61
gruler <gruler@me.com>
2024-05-30 19:38:44 -0700

    working on sliders

--- src/public/zipcodetest.js
+++ src/public/zipcodetest.js
@@ -169,8 +169,12 @@ function populateCitiesCounties() {
 };
 
 function populateCountyZipCodes() {
-    const stateSelect = document.getElementById('stateSelect').value;
     const countySelect = document.getElementById('countySelect').value;
+    if (countySelect === 'Select a County') {
+        citySelect.disabled = false;
+        return;
+    }
+    const stateSelect = document.getElementById('stateSelect').value;
     const citySelect = document.getElementById('citySelect');       
     citySelect.disabled = true;
 
@@ -194,8 +198,15 @@ function populateCountyZipCodes() {
 };
 
 function populateCityZipCodes() {
+    const citySelect = document.getElementById('citySelect').value;
+    if (citySelect === 'Select a City') {
+        citySelect.disabled = false;
+        return;
+    }
     const stateSelect = document.getElementById('stateSelect').value;
     const citySelect = document.getElementById('citySelect').value;
+    const countySelect = document.getElementById('countySelect');       
+    countySelect.disabled = true;
     const availabeZipCodesContainer = document.getElementById("availabeZipCodesContainer");
     // const selectedZipCodesContainer = document.getElementById("selectedZipCodesContainer");
     fetch(`/get-zipcodes?stateSelect=${encodeURIComponent(stateSelect)}&citySelect=${encodeURIComponent(citySelect)}`)
