b051c6f3a425741aa985fc64d88bfadaa694a8c4
gruler <gruler@me.com>
2024-06-03 13:50:16 -0700

    working on sliders

--- src/public/profile_b.js
+++ src/public/profile_b.js
@@ -270,6 +270,7 @@ function savePropertyChanges() {
     const prequalifiedY = document.getElementById('prequalifiedY').checked;
     const prequalifiedN = document.getElementById('prequalifiedN').checked;
     const preferredLanguages = document.getElementById('preferredLanguages').value;
+    const levelOfService = document.getElementById('levelOfService').value;
     const userid = document.getElementById('userid').value;
     console.log(prequalifiedY,"Prequalified Y",prequalifiedN,"Prequalified N");
 
