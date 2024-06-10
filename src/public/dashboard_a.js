e5570ee1382e698b1617450f6fe4a36e6a345acc
gruler <gruler@me.com>
2024-06-10 14:10:26 -0700

    working on sliders

--- src/public/dashboard_a.js
+++ src/public/dashboard_a.js
@@ -80,7 +80,7 @@ function newRequestDetail(buyerid) {
                     User Zip Codes: ${request.zipCodes}<br>
                     Entered on ${request.entrytimestamp}<br>
                     `;
-                div.className = "form-row";
+                div.className = "row";
                 div.id = "buyerid" + request.buyerid;
                 detailColumn.appendChild(div);
 
