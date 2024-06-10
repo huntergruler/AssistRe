587174dbd839731b526d6c2cb144ae350b4eb1c6
gruler <gruler@me.com>
2024-06-09 19:18:38 -0700

    working on sliders

--- src/public/dashboard_a.js
+++ src/public/dashboard_a.js
@@ -18,7 +18,7 @@ function getNewRequests() {
                 data.forEach(request => {
                     console.log(request);
                     const div = document.createElement("div");
-                    div.textContent = request;
+                    div.textContent = request.buyerType + ' ' + request.price_min + ' ' + request.price_max  + ' ' + request.prequalified + ' ' + request.timeframe;
                     div.className = "form-group";
                     div.onclick = function () {
                         this.classList.toggle("selected");
