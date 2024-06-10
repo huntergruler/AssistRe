831f2db46e503bb167960ac8d3f3071ffc0f92f8
gruler <gruler@me.com>
2024-06-10 10:45:53 -0700

    working on sliders

--- src/routes/index.js
+++ src/routes/index.js
@@ -115,16 +115,23 @@ router.get('/dashboard_a', (req, res) => {
 
 // Route to get the buyer's profile
 router.get('/getNewRequests', (req, res) => {
-  if (!req.session.user) {
+  const { buyerid } = req.body
+    if (!req.session.user) {
     req.session.message = 'Please login to access your Profile';
     res.redirect('/');
   }
   else {
     const userid = req.session.userid;
+    if ( buyerid == null ) {
     const query = `select bam.agentid, bam.buyerid, bam.bathrooms_min, bam.bedrooms_min, bam.buyerType, bam.preferredLanguages, bam.prequalified, format(bam.price_min,0) price_min, format(bam.price_max,0) price_max, bam.propertyType, bam.squareFootage_min, bam.squareFootage_max, bam.timeFrame, DATE_FORMAT(bam.entrytimestamp, '%m/%d/%Y %r') entrytimestamp, bam.zipCodes
                      from AgentBuyerMatch bam
-                    where bam.agentid = ?`;
-    db.query(query, [userid], (error, results) => {
+                    where bam.agentid = ? and ?`;}
+    else {
+    const query = `select bam.agentid, bam.buyerid, bam.bathrooms_min, bam.bedrooms_min, bam.buyerType, bam.preferredLanguages, bam.prequalified, format(bam.price_min,0) price_min, format(bam.price_max,0) price_max, bam.propertyType, bam.squareFootage_min, bam.squareFootage_max, bam.timeFrame, DATE_FORMAT(bam.entrytimestamp, '%m/%d/%Y %r') entrytimestamp, bam.zipCodes
+                      from AgentBuyerMatch bam
+                      where bam.agentid = ?
+                      and bam.buyerid = ?`;}
+    db.query(query, [userid,buyerid], (error, results) => {
       if (error) {
         console.error('Error fetching buyer profile:', error);
         return res.status(500).send('Server error');
