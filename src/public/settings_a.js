239cae64c2fcdfa5c7d383df59abbf336b79b95c
gruler <gruler@me.com>
2024-06-11 07:28:22 -0700

    working on sliders

---
+++ src/public/settings_a.js
@@ -0,0 +1,19 @@
+function openTab(evt, tabName) {
+    // Hide all tab content elements
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Remove "active" class from all tab buttons
+    var tablinks = document.getElementsByClassName("tablinks");
+    for (var i = 0; i < tablinks.length; i++) {
+      tablinks[i].classList.remove("active");
+    }
+  
+    // Show the selected tab content
+    document.getElementById(tabName).style.display = "block";
+  
+    // Add "active" class to the clicked tab button
+    evt.currentTarget.classList.add("active");
+  }
