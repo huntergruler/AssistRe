dbb201679b288963dd5ab681e8b3eefadf6eecd1
gruler <gruler@me.com>
2024-06-10 22:53:35 -0700

    working on sliders

--- src/routes/index.js
+++ src/routes/index.js
@@ -782,7 +782,7 @@ router.post('/api/saveoffer', (req, res) => {
   const userid = req.session.userid;
   const {buyerid, offerType, compensationType, levelOfService, compensationAmount, retainerFee, retainerCredit, lengthOfService, expirationCompensation, expirationCompTimeFrame, offerDesc} = req.body;
   console.log('Offer:', userid, buyerid, offerType, compensationType, levelOfService, compensationAmount, retainerFee, retainerCredit, lengthOfService, expirationCompensation, expirationCompTimeFrame, offerDesc);
-  insertQuery = 'INSERT INTO AgentOffers (agentid, buyerid, offerType, compensationType, levelOfService, compensationAmount, retainerFee, retainerCredited, lengthOfService, expirationCompensation, expirationCompTimeFrame, offerDesc) values (?,?,?,?,?,?,?,?,?,?,?,?)';
+  insertQuery = 'REPLACE INTO AgentOffers (agentid, buyerid, offerType, compensationType, levelOfService, compensationAmount, retainerFee, retainerCredited, lengthOfService, expirationCompensation, expirationCompTimeFrame, offerDesc) values (?,?,?,?,?,?,?,?,?,?,?,?)';
   db.query(insertQuery, [userid, buyerid, offerType, compensationType, levelOfService, compensationAmount, retainerFee, retainerCredit, lengthOfService, expirationCompensation, expirationCompTimeFrame, offerDesc], (error, result) => {
     if (error) {
       console.error('Error saving offer:', error);
