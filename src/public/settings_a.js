60b47432c45ab2cf27777fb3bd243d2fb21f1f57
gruler <gruler@me.com>
2024-06-11 09:01:16 -0700

    working on sliders

--- src/public/settings_a.js
+++ src/public/settings_a.js
@@ -33,14 +33,14 @@ function savetest() {
     const levelOfService = document.getElementById('levelOfService').value;
     const compensationType = document.getElementById('compensationType').value;
     const compensationAmount = document.getElementById('compensationAmount').value;
-    // const retainerFee = document.getElementById('retainerFee').value;
-    // const lengthOfService = document.getElementById('lengthOfService').value;
-    // const expirationCompTimeFrame = document.getElementById('expirationCompTimeFrame').value;
+    const retainerFee = document.getElementById('retainerFee').value;
+    const lengthOfService = document.getElementById('lengthOfService').value;
+    const expirationCompTimeFrame = document.getElementById('expirationCompTimeFrame').value;
     // const expirationCompensation = document.getElementById('expirationCompensation').value;
     // const offerDesc = document.getElementById('offerDesc').value;
     // const radioButtons = document.querySelectorAll('input[name="retainerCredit"]');
     // Get values from input fields
-    console.log('offerType: ' + offerType, 'levelOfService: ' + levelOfService, 'compensationType: ' + compensationType, 'compensationAmount: ' + compensationAmount);
+    console.log('offerType: ' + offerType, 'levelOfService: ' + levelOfService, 'compensationType: ' + compensationType, 'compensationAmount: ' + compensationAmount, 'retainerFee: ' + retainerFee, 'lengthOfService: ' + lengthOfService, 'expirationCompTimeFrame: ' + expirationCompTimeFrame);
     
 }   
 
