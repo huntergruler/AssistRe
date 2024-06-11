3551cb46cf59c350a121d78c9078c4b594afda3a
gruler <gruler@me.com>
2024-06-10 22:12:37 -0700

    working on sliders

--- src/public/dashboard_a.js
+++ src/public/dashboard_a.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
     populateOfferTypes();
     populateCompensationTypes();
     const offerForm = document.getElementById('offerForm');
-    // offerForm.style.display = 'none';
-    // document.querySelector('#newRequestDetail').innerHTML = '<c><br><strong> <--- Select a buyer request to view details </strong><br><br></c>';
+    offerForm.style.display = 'none';
+    document.querySelector('#newRequestDetail').innerHTML = '<c><br><strong> <--- Select a buyer request to view details </strong><br><br></c>';
     // var time_zone_offset = new Date().getTimezoneOffset(); // in minutes
     // var time_zone = Date().time_zone;
     // SELECT DATE_FORMAT(CONVERT_TZ(your_timestamp_column, '+00:00', @user_time_zone), '%m/%d/%Y %h:%i:%s %p') AS formatted_timestamp
