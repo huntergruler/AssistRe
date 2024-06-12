86753ae87c575b0db0e3e3559c88d99600186714
Hunter Gruler <hgruler@purdue.edu>
2024-06-12 14:50:41 -0700

    changes

--- src/public/dashboard_a.js
+++ src/public/dashboard_a.js
@@ -70,7 +70,7 @@ function getRequests(datatype, element) {
                     Prequalified? ${request.prequalified}<br>
                     Purchase Timeline: ${request.timeFrame}`;
                     div.addEventListener('click', () => selectRequest(request.buyerid, request.buyerrequestid));
-                    div.className = "form-row container-left col-md-6 align-self-end";
+                    div.className = "form-row container-left col-md-9 align-self-end";
                     div.id = "buyerid" + request.buyerid;
                     div.onclick = function () {
                         this.classList.toggle("selected");
