de347c9eabb37b31dd8a5f51423bb705b9a870e2
gruler <gruler@me.com>
2024-06-15 13:19:10 -0700

    working on sliders

--- src/routes/index.js
+++ src/routes/index.js
@@ -341,7 +341,6 @@ router.get('/dashboard_b', (req, res) => {
   }
   else {
     const userid = req.session.userid;
-
     const query = `select b.userid, b.firstName, b.lastName, b.address, b.city, b.state, b.userid, b.zip, b.email, b.phoneNumber, 
                           ifnull(brd.bathrooms_min,0) bathrooms_min, ifnull(brd.bathrooms_max,0) bathrooms_max, ifnull(brd.bedrooms_min,0) bedrooms_min, ifnull(brd.bedrooms_max,0) bedrooms_max, brd.buyerType, ifnull(brd.preferredLanguages,'') preferredLanguages, if(brd.prequalified="Y",'Yes','No') prequalified, ifnull(brd.price_min,0) price_min, 
                           ifnull(brd.price_max,0) price_max, ifnull(brd.propertyType,'') propertyType, ifnull(brd.squareFootage_min,0) squareFootage_min, ifnull(brd.squareFootage_max,0) squareFootage_max, ifnull(brd.timeFrame,'') timeFrame, brd.prequalifiedFile, brd.levelOfService,
