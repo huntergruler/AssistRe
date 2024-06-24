b7d8b46a5470cfb3970eadaad590c06c90e7cd37
gruler <gruler@me.com>
2024-06-24 15:32:09 -0700

    working on contact exchange

--- src/public/dashboard_b.js
+++ src/public/dashboard_b.js
@@ -361,7 +361,7 @@ function offerDetail(agentid, buyeragentmatchid) {
                 const div = document.createElement("div");
                 div.className = "form-row agentcontact-container";
                 div.id = "agentid" + request.agentid;
-                var innerHTMLtext = `<div class="flex-fill offersummary">
+                var innerHTMLtext = `<div form-row col-md-12><div class="flex-fill offersummary">
                 <u>Agent ${request.dispIdentifier} Full Offer Details</u><br>
                 Offer Type: <b>${request.offerType}</b><br>
                 Level of Service: <b>${request.levelOfService}</b><br>
@@ -379,7 +379,7 @@ function offerDetail(agentid, buyeragentmatchid) {
                     innerHTMLtext += `<button type="button" id="agentinfo-button" class="agentinfo-button" 
                                                onclick="requestAgentInfo(${request.agentid},${buyeragentmatchid});">
                                        Request Agent Contact Info
-                                       </button>
+                                       </button></div>
                                        `;
                 } else if (request.agentInfoRequested == 1) {
                     innerHTMLtext += `<button type="button" id="agentinfo-button" class="agentinfo-button disabled"
