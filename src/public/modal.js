a8e68676eedae0ef27af25c4dbd7ab699ff919f8
gruler <gruler@me.com>
2024-06-25 07:38:13 -0700

    working on contact exchange

--- src/public/modal.js
+++ src/public/modal.js
@@ -5,14 +5,14 @@ $(document).ready(function () {
 
 // Show the modal on page load
 window.onload = function () {
-    document.getElementById('modal-overlay').style.display = 'flex';
+    document.getElementById('messageModal').style.display = 'flex';
 };
 
 // Close the modal
 document.getElementById('modal-close').onclick = function () {
-    document.getElementById('modal-overlay').style.display = 'none';
+    document.getElementById('messageModal').style.display = 'none';
 };
 
 document.getElementById('modal-close-footer').onclick = function () {
-    document.getElementById('modal-overlay').style.display = 'none';
+    document.getElementById('messageModal').style.display = 'none';
 };
