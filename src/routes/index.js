d147d86c3ea9f03c745ba46e03cd0923e0dff612
gruler <gruler@me.com>
2024-06-25 08:41:54 -0700

    working on contact exchange

--- src/routes/index.js
+++ src/routes/index.js
@@ -1610,7 +1610,7 @@ router.get('/confirmcontact', (req, res) => {
         if (error) {
           return res.status(500).json({ error: 'Internal server error' });
         }
-        res.redirect('/', { message: 'Contact information confirmed' });
+        res.render('/', { message: 'Contact information confirmed' });
       });
     } else {
       res.status(404).json({ error: 'Invalid token' });
