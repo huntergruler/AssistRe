e41691060fc66c88fe2b9c2b832ee3568182c7a4
gruler <gruler@me.com>
2024-06-27 11:11:33 -0700

    working on contact exchange

--- src/public/profile_b.js
+++ src/public/profile_b.js
@@ -312,7 +312,6 @@ function getBuyerTypes() {
                 if (buyerTypesData) {
                     if (buyerTypesData.includes(item.buyertypeid)) {
                         checked = 'checked';
-                        console.log('checked', checked, item.buyertypeid);
                     }
                     else {
                         checked = null;
@@ -325,26 +324,5 @@ function getBuyerTypes() {
                     </div>`;
             }
             );
-
-            //     // Create a checkbox element
-            //     const checkbox = document.createElement('input');
-            //     checkbox.type = 'checkbox';
-            //     checkbox.name = 'buyerType';
-            //     checkbox.value = item.buyertypeid;
-            //     checkbox.id = item.buyertypeid;
-            //     // Check the checkbox if the buyer type is in the buyerTypesData array
-            //     if (buyerTypesData) {
-            //         checkbox.checked = buyerTypesData.includes(item.buyertypeid);
-            //     }
-            //     // Create a label element
-            //     const label = document.createElement('label');
-            //     label.htmlFor = item.buyertypeid;
-            //     label.className = 'checkbox-label';
-            //     label.innerHTML = '&nbsp;' + item.buyerType + '&nbsp;';
-
-            //     // Append the checkbox and label to the div
-            //     buyerTypeCheckbox.appendChild(checkbox);
-            //     buyerTypeCheckbox.appendChild(label);
-            // });
         })
 }
